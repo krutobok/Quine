7c9602a82c9fb9338f4105365581e32354031ab9
Alexander <sasha020604336618@gmail.com>
2023-03-05 16:55:19 +0200

    1.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,32 @@
 import { useState } from 'react'
 import './App.css'
-import InputForm from './components/Quine/InputForm'
-import Quine from './components/Quine/Quine'
+import QuineComponent from './components/Quine/QuineComponent'
+import VeitchApp from './components/Veitch/VeitchApp'
 
 function App() {
-  const [startCombinations, setStartCombination] = useState('')
-  const [isCorrect, setIsCorrect] = useState(true)
-  const [isQuineShown, setIsQuineShown] = useState(false)
-  const formOnSubmitHandler = (inputValue) => {
-    const regex = /^[0-9\s]+$/
-    if (regex.test(inputValue.trim())) {
-      setStartCombination(inputValue)
-      setIsQuineShown(true)
-      setIsCorrect(true)
-    } else {
-      setIsCorrect(false)
-    }
-  }
-
-  let combinationIds = startCombinations.trim().split(' ')
-  combinationIds = startCombinations
-    ? combinationIds.map((el) => parseInt(el))
-    : []
-  const size = startCombinations
-    ? Math.max(...combinationIds).toString(2).length
-    : 0
-  combinationIds = combinationIds.map((el) =>
-    Array.from(el.toString(2).padStart(size, '0'))
-  )
+  const [currentMethod, setCurrentMethod] = useState('quine')
   return (
     <div className="App">
-      <h1>Розрахунок МДНФ методом Квайна</h1>
-      <InputForm onSubmitHandler={formOnSubmitHandler} />
-      {isQuineShown && isCorrect && (
-        <Quine size={size} startCombinations={combinationIds} />
-      )}
-      {!isCorrect && (
-        <p>Введіть коректні вхідні дані, а саме цифри через пробіл</p>
-      )}
+      <nav style={{ display: 'flex', gap: '10px' }}>
+        <button
+          onClick={() => setCurrentMethod('quine')}
+          className={`switch-method ${
+            currentMethod === 'quine' ? 'active' : ''
+          }`}
+        >
+          Метод Квайна
+        </button>
+        <button
+          onClick={() => setCurrentMethod('veitch')}
+          className={`switch-method ${
+            currentMethod === 'veitch' ? 'active' : ''
+          }`}
+        >
+          Таблиці Вейча
+        </button>
+      </nav>
+      {currentMethod === 'quine' && <QuineComponent />}
+      {currentMethod === 'veitch' && <VeitchApp />}
     </div>
   )
 }
